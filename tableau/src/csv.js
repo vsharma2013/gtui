e0d6e2032212514a79d004d2e46352a061635990
Vishal Sharma <v.sharma5@partner.samsung.com>
2015-04-23 20:48:15 +0530

    csv module

---
+++ tableau/src/csv.js
@@ -0,0 +1,65 @@
+
+var utils = require('./utils');
+var sales = require('./sales');
+
+var rootDir = 'P:\\zlast2weeks\\gtui\\tableau\\';
+
+var FILE_NAME = {
+	CUSTOMER_CSV : rootDir + 'customers.csv',
+	PRODUCTS_CSV : rootDir + 'products.csv',
+	REGIONS_CSV : rootDir + 'regions.csv',
+	ADDRESS_CSV : rootDir + 'address.csv',
+	TIMES_CSV : rootDir + 'times.csv',
+	SALES_CSV : rootDir + 'sales.csv'
+}
+
+
+
+function createCustomerCsv(customers){
+	var custCsvStr = utils.convertObjectsToCSV(customers);
+	utils.saveCSVStringToFile(FILE_NAME.CUSTOMER_CSV, custCsvStr);
+}
+
+function createProductsCsv(products){
+	var prodCsvStr = utils.convertObjectsToCSV(products);
+	utils.saveCSVStringToFile(FILE_NAME.PRODUCTS_CSV, prodCsvStr);	
+	console.log('*** Total products =  ' + products.length);
+}
+
+function createRegionsCsv(regions){
+	var regionCsvStr = utils.convertObjectsToCSV(regions);
+	utils.saveCSVStringToFile(FILE_NAME.REGIONS_CSV, regionCsvStr);	
+	console.log('*** Total regions =  ' + regions.length);
+}
+
+function createAdressCsv(addForCustNReg){
+	var addressCsvStr = utils.convertObjectsToCSV(addForCustNReg);
+	utils.saveCSVStringToFile(FILE_NAME.ADDRESS_CSV, addressCsvStr);	
+	console.log('*** Total addresses =  ' + addForCustNReg.length);
+}
+
+function createTimesCsv(times){
+	var timeCsvStr = utils.convertObjectsToCSV(times);
+	utils.saveCSVStringToFile(FILE_NAME.TIMES_CSV, timeCsvStr);
+	console.log('*** Total times = ', times.length);
+}
+
+function createSalesCsv(products, addForCustNReg, times){
+	var salesForProductsNAddresses = sales.getSalesForProductsAddressesAndTimes(products, addForCustNReg, times);
+	var salesCsvStr = utils.convertObjectsToCSV(salesForProductsNAddresses);
+	utils.saveCSVStringToFile(FILE_NAME.SALES_CSV, salesCsvStr);
+	console.log('*** Total sales = ', salesForProductsNAddresses.length);
+}
+
+function createCsvFiles(customers, products, regions, addForCustNReg, times){
+	createCustomerCsv(customers);
+	createProductsCsv(products);
+	createRegionsCsv(regions);
+	createAdressCsv(addForCustNReg);
+	createTimesCsv(times);
+	createSalesCsv(products, addForCustNReg, times);
+}
+
+module.exports = {
+	createCsvFiles : createCsvFiles
+}
--- tableau/src/server.js
+++ tableau/src/server.js
@@ -4,7 +4,7 @@ var products = require('./products');
 var regions = require('./regions');
 var addresses = require('./address');
 var times = require('./times');
-var sales = require('./sales');
+
 
 customers = utils.shuffleArray(customers);
 products = utils.shuffleArray(products);
@@ -13,58 +13,10 @@ times = utils.shuffleArray(times);
 var addForCustNReg = addresses.getAddressesForCustomersAndRegions(customers, regions);
 addForCustNReg = utils.shuffleArray(addForCustNReg);
 
-var rootDir = 'P:\\zlast2weeks\\gtui\\tableau\\';
 
-var FILE_NAME = {
-	CUSTOMER_CSV : rootDir + 'customers.csv',
-	PRODUCTS_CSV : rootDir + 'products.csv',
-	REGIONS_CSV : rootDir + 'regions.csv',
-	ADDRESS_CSV : rootDir + 'address.csv',
-	TIMES_CSV : rootDir + 'times.csv',
-	SALES_CSV : rootDir + 'sales.csv'
+function createCsvFiles(customers, products, regions, addForCustNReg, times){
+	var csv = require('./csv');
+	csv.createCsvFiles(customers, products, regions, addForCustNReg, times);
 }
 
-
-
-function createCustomerCsv(){
-	var custCsvStr = utils.convertObjectsToCSV(customers);
-	utils.saveCSVStringToFile(FILE_NAME.CUSTOMER_CSV, custCsvStr);
-}
-
-function createProductsCsv(){
-	var prodCsvStr = utils.convertObjectsToCSV(products);
-	utils.saveCSVStringToFile(FILE_NAME.PRODUCTS_CSV, prodCsvStr);	
-	console.log('*** Total products =  ' + products.length);
-}
-
-function createRegionsCsv(){
-	var regionCsvStr = utils.convertObjectsToCSV(regions);
-	utils.saveCSVStringToFile(FILE_NAME.REGIONS_CSV, regionCsvStr);	
-	console.log('*** Total regions =  ' + regions.length);
-}
-
-function createAdressCsv(){
-	var addressCsvStr = utils.convertObjectsToCSV(addForCustNReg);
-	utils.saveCSVStringToFile(FILE_NAME.ADDRESS_CSV, addressCsvStr);	
-	console.log('*** Total addresses =  ' + addForCustNReg.length);
-}
-
-function createTimesCsv(){
-	var timeCsvStr = utils.convertObjectsToCSV(times);
-	utils.saveCSVStringToFile(FILE_NAME.TIMES_CSV, timeCsvStr);
-	console.log('*** Total times = ', times.length);
-}
-
-function createSalesCsv(){
-	var salesForProductsNAddresses = sales.getSalesForProductsAddressesAndTimes(products, addForCustNReg, times);
-	var salesCsvStr = utils.convertObjectsToCSV(salesForProductsNAddresses);
-	utils.saveCSVStringToFile(FILE_NAME.SALES_CSV, salesCsvStr);
-	console.log('*** Total sales = ', salesForProductsNAddresses.length);
-}
-
-createCustomerCsv();
-createProductsCsv();
-createRegionsCsv();
-createAdressCsv();
-createTimesCsv();
-createSalesCsv();
+createCsvFiles(customers, products, regions, addForCustNReg, times);
