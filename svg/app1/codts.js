55e60203a3418c10f7b72755e8d6168aba6b847e
Vishal Sharma <vishal2033@gmail.com>
2015-06-02 20:51:26 +0530

    added utils.js

--- svg/app1/codts.js
+++ svg/app1/codts.js
@@ -124,51 +124,6 @@ function getPathForFrame(f){
 	return path;
 }
 
-function getRandomItemFromArray(arr){
-	return arr[this.getRandomIntFromInterval(0,arr.length-1)];
-}
-
-function getRandomIntFromInterval(min, max){
-	return Math.floor(Math.random()*(max-min+1)+min);
-}
-
-
-function Path(){
-	this.content = [];
-}
-
-Path.prototype.moveTo = function(x,y){
-	this.content.push({
-		moveTo : {
-			x : x,
-			y : y
-		}
-	});
-}
-
-Path.prototype.lineTo = function(x, y){
-	this.content.push({
-		lineTo : {
-			x : x,
-			y : y
-		}
-	});
-}
-
-Path.prototype.toString = function(){
-	var path = '';
-	this.content.forEach(function(p){
-		if(p.moveTo){
-			path += 'M ' + p.moveTo.x + ' ' + p.moveTo.y + ' ';
-		}
-		else if(p.lineTo){
-			path += 'L ' + p.lineTo.x + ' ' + p.lineTo.y + ' ';
-		}
-		
-	});
-	path += ' Z';
-	return path;
-}
 
 var isRotating = false;
 
