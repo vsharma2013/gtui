60a903bce773c32c8bfa63188948abe7a85aae07
Vishal Sharma <vishal2033@gmail.com>
2015-08-18 21:29:23 +0530

    ngapp1 directive per style guide

--- ngapps/app1/web/src/controllers/controllers.js
+++ ngapps/app1/web/src/controllers/controllers.js
@@ -1,20 +1,21 @@
-var phonecatControllers = angular.module('phonecatControllers', []);
+(function(){
+	angular
+		.module('phonecatControllers', [])
+		.controller('PhoneListCtrl', PhoneListCtrl)
+		.controller('PhoneDetailCtrl', PhoneDetailCtrl);
 
-phonecatControllers.controller('PhoneListCtrl', PhoneListCtrl);
-phonecatControllers.controller('PhoneDetailCtrl', PhoneDetailCtrl);
+	PhoneListCtrl.$inject =['$scope', 'Phone', 'myEmptyService'];
+	
+	function PhoneListCtrl($scope, Phone, myEmptyService){
+		$scope.phones = Phone.query();
+	 	$scope.orderProp = 'name';
+	}
 
-function PhoneListCtrl($scope, Phone, myEmptyService){
-	$scope.phones = Phone.query();
- 	$scope.orderProp = 'name';
-}
-
-PhoneListCtrl.$inject =['$scope', 'Phone', 'myEmptyService'];
-
-
-function PhoneDetailCtrl($scope, $routeParams, Phone, myEmptyService){
-	$scope.phone = Phone.get({phoneId: $routeParams.phonesId}, function(phone) {
-	    $scope.mainImageUrl = phone.images[0];
-	});
-}
-
-PhoneDetailCtrl.$inject = ['$scope', '$routeParams', 'Phone', 'myEmptyService'];
+	PhoneDetailCtrl.$inject = ['$scope', '$routeParams', 'Phone', 'myEmptyService'];
+	
+	function PhoneDetailCtrl($scope, $routeParams, Phone, myEmptyService){
+		$scope.phone = Phone.get({phoneId: $routeParams.phonesId}, function(phone) {
+		    $scope.mainImageUrl = phone.images[0];
+		});
+	}	
+})();
