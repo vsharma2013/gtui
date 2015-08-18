e288e07cd2fa069a3c6343f287922b39aa67c79b
Vishal Sharma <vishal2033@gmail.com>
2015-08-18 12:18:19 +0530

    ngapp1 directive per style guide

--- ngapps/app1/web/src/views/tabs/tabs.js
+++ ngapps/app1/web/src/views/tabs/tabs.js
@@ -1,41 +1,45 @@
-angular
-.module('tabsApp', [])
-.directive('myTabs', function(){
-	return {
-		restrict : 'E',
-		transclude : true,
-		scope : {},
-		controller : function($scope){
-			var panes = $scope.panes = [];
-			$scope.select = function(pane){
-				angular.forEach(panes, function(pane){
-					pane.selected = false;
-				});
-				pane.selected = true;
-			}
-			this.addPane = function(pane){
-				if(panes.length === 0){
-					$scope.select(pane);
+(function(){
+	angular
+	.module('tabsApp', [])
+	.directive('myTabs', myTabs)
+	.directive('myPane', myPane);
+	
+	function myTabs(){
+		return {
+			restrict : 'E',
+			transclude : true,
+			scope : {},
+			controller : function($scope){
+				var panes = $scope.panes = [];
+				$scope.select = function(pane){
+					angular.forEach(panes, function(pane){
+						pane.selected = false;
+					});
+					pane.selected = true;
 				}
-				panes.push(pane);
-			}
-		},
-		templateUrl : 'src/views/tabs/tabs.html'
-	}	
-})
-.directive('myPane', function(){
-	return {
-		require : '^myTabs',
-		restrict : 'E',
-		transclude : true,
-		templateUrl : 'src/views/tabs/pane.html',
-		scope : {
-			title : '@'
-		},
-		link : function(scope, element, attrs, tabsController){
-			tabsController.addPane(scope);
+				this.addPane = function(pane){
+					if(panes.length === 0){
+						$scope.select(pane);
+					}
+					panes.push(pane);
+				}
+			},
+			templateUrl : 'src/views/tabs/tabs.html'
 		}
-	};	
-});
-
-
+	} 
+	
+	function myPane(){
+		return {
+			require : '^myTabs',
+			restrict : 'E',
+			transclude : true,
+			templateUrl : 'src/views/tabs/pane.html',
+			scope : {
+				title : '@'
+			},
+			link : function(scope, element, attrs, tabsController){
+				tabsController.addPane(scope);
+			}
+		}
+	};
+})();
